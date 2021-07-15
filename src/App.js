7cf047fc727c8a8c3bc7d53bb9f9896b86d2d824
Olha-Grim <olha.hrymchak@prea.ws>
2021-07-15 14:31:01 +0300

    saved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 import Box from "test-vendor-2";
+import Divider from "test-vendor-2";
+import Button from "test-vendor-2";
 
 function App() {
   return (
     <div>
-      {<Box/>}
+      <h1>hiiiii</h1>
+      <Button type="primary">Hello2</Button>
     </div>
   );
 }
